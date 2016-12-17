dde972ad3b869316a0f9ffa977de463d0e212aa6
Guillaume LORMIER <eagle59@gmail.com>
2016-12-17 16:03:57 +0100

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Resto CNAM</title>
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
 	<meta name="keywords" content="Lille, Roubaix, Tourcoing, Wasquehal, Marcq-en-Baroeul, Bondues, La Madeleine, Wattrelos, Croix, restaurant, restaurants, manger, repas, gastronomie, gastronomique, végétarien, vegetarien, végétalien, végétalien, vegan, halal, kacher, cachère, cacher, kocher">
-	  <link href="css/Resto_Cnam.css" rel="stylesheet" type="text/css">
+	<link href="css/Resto_Cnam.css" rel="stylesheet" type="text/css">
 </head>
-<?php  //include('../inc/header.php'); ?>
+<?php  include('../inc/header.php'); ?>
 <body>
-<img class="display" src="images/Toine_Hermsen_interieur.jpg">
-&nbsp;
-&nbsp;
-
+	<img class="display" src="images/Toine_Hermsen_interieur.jpg" width="80%">
+	<p id="news">LES NEWS DE LA SEMAINE</p>
+	<marquee behavior="Scroll" id="marquee" >lorem ipsum</marquee>
 </body>
 </html>
